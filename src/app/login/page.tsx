363dd937d0c2aff06a2419eb2f8b3fdb096ac4e7
muhammadzee343 <muhammad.zee343@gmail.com>
2025-03-22 13:08:41 +0500

    n

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,15 @@
+"use client";
 import React from "react";
 // import Login from "@/components/ui/Login/login";
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-const Login = dynamic(() => import('../../components/ui/Login/login'), {
+const Login = dynamic(() => import("../../components/ui/Login/login"), {
   ssr: false,
-})
+});
 export default function LoginScreen() {
   return (
-    <Login />
+    <div className="min-h-screen">
+      <Login />
+    </div>
   );
 }
